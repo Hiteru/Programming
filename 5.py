042d5a68ba10df5207fb210bd900254e3bc9c07b
Hiteru <55487374+Hiteru@users.noreply.github.com>
2019-09-24 21:08:43 +0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,16 @@
+a = int(input("Введите первое число: "));
+b = int(input("Введите второе число: "));
+
+while (a * b == 0):
+	
+	print("Число равно нулю");
+	a = int(input("Введите первое число: "));
+	b = int(input("Введите второе число: "));	
+ 	
+else:
+	print(abs(a)+abs(b))
+	print(abs(a)-abs(b))
+	print(abs(a)*abs(b))
+	print(abs(a)/abs(b))
+ 		
+ 	
